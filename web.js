8fe544bb376249fc2423b524bf22a5c47f0cdae0
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-11-22 19:40:56 +0100

    BetterCode

--- web.js
+++ web.js
@@ -1,23 +1,23 @@
 const settingBtn = document.getElementById("settings-tab");
-if (settingBtn != null) {
+if (settingBtn) {
   settingBtn.click();
 }
 setTimeout(function () {
   // click Delete this repository
   const deleteBtn = document.getElementById("dialog-show-repo-delete-menu-dialog");
-  if (deleteBtn != null) {
+  if (deleteBtn) {
     deleteBtn.click();
     // click I want to delete this repository
     const deleteBtn2 = document.getElementById("repo-delete-proceed-button");
-    if (deleteBtn2 != null) {
+    if (deleteBtn2) {
       deleteBtn2.click();
       // click I have read and ...
       const deleteBtn3 = document.getElementById("repo-delete-proceed-button");
-      if (deleteBtn3 != null) {
+      if (deleteBtn3) {
         deleteBtn3.click();
         // set the input validation and click delete
         const verField = document.getElementById("verification_field");
-        if (verField != null) {
+        if (verField) {
           const verText = verField.dataset.repoNwo;
           verField.value = verText;
           const deleteFinalBtn = document.getElementById("repo-delete-proceed-button");
